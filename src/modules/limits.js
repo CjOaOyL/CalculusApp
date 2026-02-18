5697f1e6634514af7b0dd24f402be6d007e1e53f
jaqua <jaqua@example.com>
2026-02-17 21:46:02 -0500

    Initial import: Calculus Teaching App

---
+++ src/modules/limits.js
@@ -0,0 +1,9 @@
+// Module: Limits & Continuity
+
+export function explainLimits() {
+    return `A limit is the value that a function approaches as the input approaches some value.`;
+}
+
+export function continuityExample() {
+    return `A function is continuous if it has no breaks, jumps, or holes.`;
+}
